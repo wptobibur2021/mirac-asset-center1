285b327393e167baca0df6de46a16268589106b3
Md Tobibur Rohman <tobibur.info@gmail.com>
2024-10-25 02:17:45 +0600

    Completed desing

--- components/partials/header/Menu.tsx
+++ components/partials/header/Menu.tsx
@@ -3,12 +3,12 @@ import MenuItem from "./MenuItem";
 const Menu = () => {
   return (
     <div>
-      <ul className="flex md:flex-row flex-col items-center gap-x-10 text-white">
+      <ul className="flex md:flex-row flex-col items-center md:gap-x-10 gap-y-3 text-gray-800  md:text-white">
         <MenuItem href="/" name="센터원" />
-        <MenuItem href="/" name="이용안내" />
-        <MenuItem href="/" name="서비스소개" />
-        <MenuItem href="/" name="임대문의" />
-        <MenuItem href="/" name="고객지원" />
+        <MenuItem href="#avenue" name="이용안내" />
+        <MenuItem href="#service" name="서비스소개" />
+        <MenuItem href="#event" name="임대문의" />
+        <MenuItem href="#benefit" name="고객지원" />
       </ul>
     </div>
   );
