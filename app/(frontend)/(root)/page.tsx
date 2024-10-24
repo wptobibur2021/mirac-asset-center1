9477d36d56e3d9f40a4778dd89d5a25655711586
Md Tobibur Rohman <tobibur.info@gmail.com>
2024-10-24 23:59:05 +0600

    Design completed

--- app/(frontend)/(root)/page.tsx
+++ app/(frontend)/(root)/page.tsx
@@ -2,14 +2,12 @@ import AvenueSection from "@/components/partials/Avenue/AvenueSection";
 import BenefitSection from "@/components/partials/Benefit/BenefitSection";
 import EventSection from "@/components/partials/event/EventSection";
 import FooterTop from "@/components/partials/footerTop/FooterTop";
-import HeroSection from "@/components/partials/hero/HeroSection";
 import ServicesSection from "@/components/partials/service/ServicesSection";
 import FullScreenSlider from "@/components/partials/slider/FullScreenSlider";
 
 const HomePage = () => {
   return (
     <div>
-      <HeroSection />
       <ServicesSection />
       <FullScreenSlider />
       <BenefitSection />
